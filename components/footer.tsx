fe8a3e0014ede88dde56b0c784715468169477a6
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-01-05 07:15:22 +0530

    add info to about page

---
+++ components/footer.tsx
@@ -0,0 +1,33 @@
+import { GitFork, Star } from "lucide-react";
+import Link from "next/link";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="text-gray-400 py-8 border-t">
+      <div className="container mx-auto px-4 flex flex-col items-center space-y-4">
+        <Link className="text-primary text-xl font-semibold font-mono" href="/">
+          {"<Ashish />"}
+        </Link>
+        <p className="text-xs text-center">
+          &copy; 2022-{currentYear} Ashish Agarwal
+          <br />
+          <Link
+            href="https://github.com/devashish2024/ashish.top"
+            target="_blank"
+            className="hover:underline underline-offset-2 hover:text-primary transition-all duration-75"
+          >
+            Open-source portfolio.
+          </Link>
+        </p>
+        <Link
+          className="flex text-xs items-center gap-1 hover:underline underline-offset-2 hover:text-primary transition-all duration-75"
+          href="https://github.com/devashish2024/ashish.top"
+          target="_blank"
+        >
+          429 <Star className="size-3" /> | 69 <GitFork className="size-3" />
+        </Link>
+      </div>
+    </footer>
+  );
+}
