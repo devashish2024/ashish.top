cb86750a07eb97285eba9e86cd1b88f1cc10fb1b
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-01-06 21:03:20 +0530

    make footer client ig

--- components/repostats.tsx
+++ components/repostats.tsx
@@ -1,24 +1,45 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { getRepoStats } from "@/lib/data";
 import { GitFork, Star } from "lucide-react";
 import Link from "next/link";
 
-export const dynamic = "force-dynamic";
+export const RepoStats = () => {
+  const [repoStats, setRepoStats] = useState<{
+    stars: number;
+    forks: number;
+  } | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-export async function RepoStats() {
-  try {
-    const repoStats = await getRepoStats();
-    return (
-      <Link
-        className="font-serif flex text-xs items-center gap-1 hover:underline underline-offset-2 hover:text-primary transition-all duration-75"
-        href="https://github.com/devashish2024/ashish.top"
-        target="_blank"
-      >
-        {repoStats?.stars} <Star className="size-3" /> | {repoStats?.forks}{" "}
-        <GitFork className="size-3" />
-      </Link>
-    );
-  } catch (error) {
-    console.error("Failed to fetch repo stats", error);
-    return <span>Error fetching stats</span>;
-  }
-}
+  useEffect(() => {
+    const fetchRepoStats = async () => {
+      try {
+        const stats = (await getRepoStats()) as any;
+        setRepoStats(stats);
+      } catch (err) {
+        setError("Failed to fetch repo stats");
+        console.error("Failed to fetch repo stats", err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRepoStats();
+  }, []);
+
+  if (loading) return <span>Loading...</span>;
+  if (error) return <span>{error}</span>;
+
+  return (
+    <Link
+      className="font-serif flex text-xs items-center gap-1 hover:underline underline-offset-2 hover:text-primary transition-all duration-75"
+      href="https://github.com/devashish2024/ashish.top"
+      target="_blank"
+    >
+      {repoStats?.stars} <Star className="size-3" /> | {repoStats?.forks}{" "}
+      <GitFork className="size-3" />
+    </Link>
+  );
+};
