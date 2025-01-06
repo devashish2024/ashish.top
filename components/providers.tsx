bc27ae1194f934bac4f0a010cd7095c10d4564a5
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-01-06 17:54:51 +0530

    fix build errors

--- components/providers.tsx
+++ components/providers.tsx
@@ -15,7 +15,7 @@ const Providers = async ({ children }: { children: React.ReactNode }) => {
       <div className="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-red-500 via-yellow-500 to-green-500"></div>
       <Header />
       <main className="container min-h-screen">{children}</main>
-      <Footer repoStats={repoStats} />
+      <Footer />
     </ThemeProvider>
   );
 };
