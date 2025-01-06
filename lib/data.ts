6563d2a23b7516eae41d90933c44bb8b743453b7
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-01-07 01:57:53 +0530

    little edit to not require github token

--- lib/data.ts
+++ lib/data.ts
@@ -4,19 +4,35 @@ require("dotenv").config();
 
 export async function getRepoStats() {
   try {
-    const req = await fetch(
-      "https://api.github.com/repos/devashish2024/ashish.top",
-      {
-        method: "GET",
-        headers: {
-          ContentType: "application/json",
-          Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    let req;
+    if (process.env.GITHUB_TOKEN) {
+      req = await fetch(
+        "https://api.github.com/repos/devashish2024/ashish.top",
+        {
+          method: "GET",
+          headers: {
+            ContentType: "application/json",
+            Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+          },
+          next: {
+            revalidate: 3600,
+          },
         },
-        next: {
-          revalidate: 3600,
+      );
+    } else {
+      req = await fetch(
+        "https://api.github.com/repos/devashish2024/ashish.top",
+        {
+          method: "GET",
+          headers: {
+            ContentType: "application/json",
+          },
+          next: {
+            revalidate: 3600,
+          },
         },
-      },
-    );
+      );
+    }
 
     const data = await req.json();
 
