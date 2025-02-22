fa05f92dbd0f3abeda37f7fe2686acfe6cbd7b09
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-02-23 00:36:07 +0530

    add the books link

--- components/highlights/books.tsx
+++ components/highlights/books.tsx
@@ -12,7 +12,7 @@ export default async function Books() {
         </p>
         <ActionLink
           label="List of my books"
-          href="#"
+          href={`https://www.google.com/search?sa=X&sca_esv=454e20719506dd47&udm=36&sxsrf=AHTn8zr-QB2gheT-_f8FawbBUjBjoF4_Nw:1740251060225&udm=36&q=inauthor:%22Ashish+Agarwal%22&ved=2ahUKEwiJ546y_NeLAxXESmwGHc0GNJcQ9Ah6BAgFEAY&biw=1366&bih=582&dpr=1`}
           target="_blank"
           className="text-sm inline-flex !mt-2"
         />
