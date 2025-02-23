6ad57f632868b92eca10ff14bf683f147d52524a
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-02-24 05:25:00 +0530

    imagine forgetting toaster

--- app/sign/page.tsx
+++ app/sign/page.tsx
@@ -1,13 +1,6 @@
 import Guestbook from "@/components/guestbook";
 import { Toaster } from "@/components/ui/sonner";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
@@ -19,6 +12,7 @@ export default async function Page() {
   return (
     <>
       <div className="container px-4 mb-12 mt-2 md:mt-4 space-y-4 min-h-screen">
+        <Toaster />
         <Breadcrumb>
           <BreadcrumbList>
             <BreadcrumbItem>
@@ -46,22 +40,6 @@ export default async function Page() {
           <Guestbook />
         </section>
       </div>
-      {/* <div className="container mx-auto px-4 py-8">
-      <Toaster />
-      <Card className="w-full border-0">
-        <CardHeader>
-          <CardTitle className="text-3xl font-bold">Guestbook</CardTitle>
-          <CardDescription className="max-w-2xl">
-            A simple, minimal and functional guestbook made with Next.js, Clerk,
-            Prisma and Perspective API. Feel free to leave a message below and
-            implement it into your portfolio, too!
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="space-y-6">
-          <Guestbook />
-        </CardContent>
-      </Card>
-    </div> */}
     </>
   );
 }
