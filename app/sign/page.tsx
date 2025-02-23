c32e567ab392c9ca3ba87e6ad64c014a84f12716
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-02-24 04:17:54 +0530

    ok removed sign (guestbook) for now to start over

--- app/sign/page.tsx
+++ app/sign/page.tsx
@@ -1,9 +1,3 @@
-import { Guestbook } from "@/components/guestbook/guestbook";
-
 export default function Page() {
-  return (
-    <div className="container px-4 mb-12 md:mt-8 space-y-8 min-h-screen">
-      <Guestbook />
-    </div>
-  );
+  return "";
 }
